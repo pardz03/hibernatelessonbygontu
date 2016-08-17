4850663d8332647376ede0757343709dac92dea6
john dave fajardo <fajardo_johndave@yahoo.com>
2016-08-17 16:33:41 +0800

    One to one mapping. unidirectional and bidirectional

---
+++ HibernateLessonProject/src/com/kaizen/poc/hibernate/Student.java
@@ -0,0 +1,53 @@
+package com.kaizen.poc.hibernate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+@Table(name= "STUDENTS")
+public class Student {
+
+	@Id 
+	@GeneratedValue
+	private int student_id;
+	
+	private String student_name;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="student_id")
+	private StudentDetail studentDetail;
+	
+	public StudentDetail getStudentDetail() {
+		return studentDetail;
+	}
+
+	public void setStudentDetail(StudentDetail studentDetail) {
+		this.studentDetail = studentDetail;
+	}
+
+	public int getStudent_id() {
+		return student_id;
+	}
+
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+
+	public String getStudent_name() {
+		return student_name;
+	}
+
+	public void setStudent_name(String student_name) {
+		this.student_name = student_name;
+	}
+	
+}
